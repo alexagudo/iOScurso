ddaf18f9f795bcde9ffc6ff5be8d65abfdf97bf0
Alejandro Agudo <alex@Alejandros-Mac-mini.local>
2017-08-19 09:03:40 -0700

    Se agrego app PersistenciaDatos

---
+++ Clase5/appPersistenciaDatos/appPersistenciaDatos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appPersistenciaDatos
+//
+//  Created by Alejandro Agudo on 8/18/17.
+//  Copyright © 2017 Alejandro Agudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
