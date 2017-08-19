4b1b3aff9a0562b6cd8e7da6ede48def0c117a10
Alejandro Agudo <alex@Alejandros-Mac-mini.local>
2017-08-19 10:54:23 -0700

    Se agrego app Camara

---
+++ Clase6/appCamara/appCamara/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  appCamara
+//
+//  Created by Alejandro Agudo on 8/19/17.
+//  Copyright © 2017 Alejandro Agudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //  Crear un boton que nos permita acceder a la camara
+    @IBAction func camerabuttonaction(_ sender: UIButton){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    // Esta accion nos permite acceder a la libreria de fotos de nuestros dispositivos
+    @IBAction func photolibraryaction(_ sender: UIButton){
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    // Esta accion nos permite guardar la foto que acabamos de tomar
+    @IBAction func saveaction(_ sender: UIButton){
+        let imageData = UIImageJPEGRepresentation(imageView.image!, 0.6)
+        let compressedJPEGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
+        saveNotice()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        imageView.image = image
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    func saveNotice(){
+        let alertController = UIAlertController(title: "Imagen guardada", message: "Tu imagen se ha guardado", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+        override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
